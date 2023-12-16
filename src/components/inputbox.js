ca4b7836515d1ba93790ff5b96a831a089ba1924
Ken <kg3209@nyu.edu>
2023-12-16 13:55:10 +0800

    new-input box, daisy

---
+++ src/components/inputbox.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const Inputbox = () => {
+  return (
+    <div className="text-2xl">
+      <input
+        className="m-5 w-96 h-20 text-center rounded-lg"
+        placeholder="Please insert your hand"
+      ></input>
+    </div>
+  );
+};
+export default Inputbox;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
-import { Inter } from "next/font/google";
 import Display from "@/components/Results";
-
-const inter = Inter({ subsets: ["latin"] });
+import Inputbox from "@/components/inputbox";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const [array, setArray] = useState([]);
   return (
     <div>
+      <div className="flex">
+        <Inputbox />
+        <Inputbox />
+        <button className="text-4xl">+</button>
+      </div>
       <Display
         hands={[
           ["As", "Kh"],
